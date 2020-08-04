4a358c65dabbf0e1d260952ca541f8b3ffe5dad1
FlavioSugano <flavio.sugano@gmail.com>
2020-08-04 18:48:12 -0300

    arquivos aula 6.2

---
+++ 06_2/script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  const statesBR = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  const stateList = document.getElementById('state');
+  
+
+  for (let i = 0; i < statesBR.length; i += 1) {
+    let opt = document.createElement('option');
+    opt.innerHTML = statesBR[i];
+
+    stateList.appendChild(opt);
+  }
+
+  document.getElementById('dataInicio').DatePickerX.init()
+
+}
+
