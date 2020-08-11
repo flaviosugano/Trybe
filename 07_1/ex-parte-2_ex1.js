c0343281e91a55103bac6fd095335568a94a6df7
FlavioSugano <flavio.sugano@gmail.com>
2020-08-10 22:56:30 -0300

    ex2 da parte 2

--- 07_1/ex-parte-2_ex1.js
+++ 07_1/ex-parte-2_ex1.js
@@ -1,9 +1,9 @@
 // Crie uma função que receba um número e retorne seu fatorial
 
-let fat = (num, fat = 1) => {
+let factorial = (num, fat = 1) => {
 for (let i = 1; i <= num; i += 1)
   fat = fat * i;
   return fat;
 }
 
-console.log(fat(3));
+console.log(factorial(3));
