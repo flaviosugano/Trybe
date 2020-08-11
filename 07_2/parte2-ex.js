ef9f5aa7e64ea9dbf98f558c11027d5e3b6c61a2
FlavioSugano <flavio.sugano@gmail.com>
2020-08-11 18:59:06 -0300

    parte2 ex1 e ex2

---
+++ 07_2/parte2-ex.js
@@ -0,0 +1,33 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// ex1. Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+function addTurn(lesson, key, value) {
+  lesson[key] = value;
+}
+
+// ex2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+function listObjectKeys(obj) {
+  for (let i in Object.keys(obj)) {
+    console.log(Object.keys(obj)[i]);
+  }
+}
+
+listObjectKeys(lesson3);
