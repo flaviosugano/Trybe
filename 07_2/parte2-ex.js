97c3239fe1c825825ca33b62decde8b39c888a86
FlavioSugano <flavio.sugano@gmail.com>
2020-08-11 23:45:28 -0300

    Parte2, ex5

--- 07_2/parte2-ex.js
+++ 07_2/parte2-ex.js
@@ -30,8 +30,20 @@ const listObjectKeys = (obj) => {
   }
 }
 
+listObjectKeys(lesson3)
+
 // ex3. Crie uma função para mostrar o tamanho de um objeto.
 const objectSize = (obj) => Object.keys(obj).length;
 
-console.log(objectSize(lesson2))
+// ex4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+const listObjectValues = (obj) => {
+  for (let i in Object.values(obj)) {
+    console.log(Object.values(obj)[i]);
+  }
+}
+
+// ex5. Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3.
+const allLessons = (lesson1, lesson2, lesson3) => Object.assign({}, {lesson1, lesson2, lesson3});
+
+console.log(allLessons(lesson1, lesson2, lesson3));
 
