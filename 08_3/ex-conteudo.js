f82533379bd8179333faa1682fd89cd0139f03fb
FlavioSugano <flavio.sugano@gmail.com>
2020-08-19 18:06:19 -0300

    códigos dos exercícios do conteúdo

---
+++ 08_3/ex-conteudo.js
@@ -0,0 +1,138 @@
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const sumNumbers = numbers.reduce((result, number) => result + number); 
+// O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+// formato: array.reduce(callback( acumulador, valorAtual[, index[, array]] )[, valorInicial]))
+console.log(sumNumbers); // 113
+
+// Ou seja:
+
+const getSum = (result, number) => result + number;
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+/****************************/
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+/***************************/
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 0 32 47 50 52 47 103
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 1000); // Parâmetro adicionado ao reduce: o "0"
+console.log(sumNumbers); // 113
+
+/****************************/
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => (bigger > number) ? bigger : number;
+
+const bigger = numbers.reduce(getBigger, 86);
+console.log(bigger); // 85
+
+/******************************/
+
+// faça uma função que some os números pares
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+function sumEvenNumbers(arrayNumbers) {
+  const findEven = num => num % 2 === 0;
+  const sum = (result, number) => result + number;
+  
+  return arrayNumbers
+    .filter(findEven)
+    .reduce(sum);
+}
+
+console.log(sumEvenNumbers(numbers))
+
+/****************************/
+
+// Crie uma função usando os dados dos estudantes passados anteriormente, para mostrar na tela um relatório que diz em qual matéria o estudante foi melhor.
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 }
+    ]
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' }
+    ]
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' }
+    ]
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' }
+    ]
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' }
+    ]
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' }
+    ]
+  },
+]
