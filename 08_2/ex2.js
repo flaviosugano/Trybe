50ce5fc175db97f8545ec505b040d44d8a8a536e
FlavioSugano <flavio.sugano@gmail.com>
2020-08-19 01:21:33 -0300

    ex2: ajustes no código p/ ficar mais legível

--- 08_2/ex2.js
+++ 08_2/ex2.js
@@ -91,13 +91,10 @@ const expected_result = [
 ];
 
 // ex2: Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
-function nameAndAge(array) {
-  const listAuthors = array.map((book) => ({ 
-    age: book.releaseYear - book.author.birthYear,
-    author: `${book.author.name}` 
-  }));
-
-  return listAuthors.sort((a,b) => a.age - b.age);
+function nameAndAge(arrayBooks) {
+  return arrayBooks
+    .map(book => ({ age: book.releaseYear - book.author.birthYear, author: `${book.author.name}` }))
+    .sort((person1, person2) => person1.age - person2.age);
 }
 
 assert.deepEqual(nameAndAge(books), expected_result);
