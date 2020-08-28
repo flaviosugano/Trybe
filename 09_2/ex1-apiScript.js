0bac6b16bd78b49a36e7d93257c03e49a7f50751
FlavioSugano <flavio.sugano@gmail.com>
2020-08-27 21:51:07 -0300

    Exs do conteúdo e ex1(script + html)

---
+++ 09_2/ex1-apiScript.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+
+const fetchJoke = () => {
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+};
+
+window.onload = () => fetchJoke();
+
+// fetch(API_URL, myObject)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
