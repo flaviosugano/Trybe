611f59777a6dbf61dd069f8bdd9246305614e2ed
FlavioSugano <flavio.sugano@gmail.com>
2020-08-27 22:31:46 -0300

    ex2

---
+++ 09_2/ex2.js
@@ -0,0 +1,17 @@
+const createArray = new Promise((onSuccess, onFail) => {
+  const arrNumbers = [];
+  for (let i = 0; i < 10; i += 1) {
+    arrNumbers.push(Math.floor(Math.random() * 50) + 1);
+  }
+
+  const sumArrNumbers = arrNumbers
+    .map(number => number * number)
+    .reduce((acc, cur) => acc + cur);
+
+  if (sumArrNumbers < 8000) {
+    return onSuccess(sumArrNumbers)
+  }
+  onFail(sumArrNumbers);
+})
+.then(number => console.log(`Deu certo! A soma foi de ${number}`))
+.catch(number => console.log(`Deu ruim, irmão. A soma deu ${number}`));
