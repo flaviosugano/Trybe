51384ba6657828196ef06c8b01de6c0357ce24bb
FlavioSugano <flavio.sugano@gmail.com>
2020-09-03 19:28:51 -0300

    Ex1

---
+++ 10_2/ex1.test.js
@@ -0,0 +1,13 @@
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+}
+
+describe('Testing uppercase function', () => {
+
+  it('if retrives an uppercase string', () => {
+    uppercase('xablau', (strUpperCased) => {
+      expect(strUpperCased).toBe('XABLAU');
+    })
+  });
+
+})
