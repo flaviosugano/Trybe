453f1a3ff20660f7b80d9e8a4f2e5f9d1af27502
FlavioSugano <flavio.sugano@gmail.com>
2020-09-15 22:46:37 -0300

    Exercício 1: criação do botão p/ passar p/ o próximo pokemon da lista

---
+++ 12_1/exercise-pokedex-state/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.nextPokemon = this.nextPokemon.bind(this)
+
+    this.state = {
+      position: 0
+    }
+  }
+
+  nextPokemon() {
+    this.setState((previous, _props) => {
+      if (previous.position < pokemons.length - 1) {
+        return { position: previous.position + 1 }
+      } return { position: 0 }
+    })
+  }
+
+  render() {
+    let index = this.state.position;
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <button onClick={this.nextPokemon}>Next {'>>'}</button>
+        <Pokedex pokemon={pokemons[index]} />
+      </div>
+    );
+  }
+}
+
+export default App;
