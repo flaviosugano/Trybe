2b66cd6cc4a01cd831a5a00ae898d01cf43be460
FlavioSugano <flavio.sugano@gmail.com>
2020-09-17 12:56:58 -0300

    Ex2

--- 12_1/exercise-pokedex-state/src/App.js
+++ 12_1/exercise-pokedex-state/src/App.js
@@ -4,34 +4,11 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.nextPokemon = this.nextPokemon.bind(this)
-
-    this.state = {
-      position: 0
-    }
-  }
-
-  nextPokemon() {
-    this.setState((previous, _props) => {
-      if (previous.position < pokemons.length - 1) {
-        return { position: previous.position + 1 }
-      } return { position: 0 }
-    })
-  }
-
   render() {
-    let index = this.state.position;
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <button onClick={this.nextPokemon}>Next {'>>'}</button>
-        <Pokedex pokemon={pokemons[index]} />
-        <div className="filterButtons">
-          <div className="typeButton">Fire</div>
-          <div className="typeButton">Psychic</div>
-        </div>
+        <Pokedex pokemons={pokemons} />
       </div>
     );
   }
--- 12_1/exercise-pokedex-state/src/Pokedex.js
+++ 12_1/exercise-pokedex-state/src/Pokedex.js
@@ -2,11 +2,48 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+  constructor() {
+    super()
+    this.nextPokemon = this.nextPokemon.bind(this)
+    this.typeSelect = this.typeSelect.bind(this)
+
+    this.state = {
+      position: 0,
+      type: 'Fire'
+    }
+  }
+
+  nextPokemon(pokemonList) {
+    this.setState((previous, _props) => {
+      if (previous.position < pokemonList.length - 1) {
+        return { position: previous.position + 1 }
+      } return { position: 0 }
+    })
+  }
+
+  typeSelect(event) {
+    const choosenType = event.target.id;
+    this.setState((previous) => ({
+      type: choosenType
+    }));
+  }
+
   render() {
-    const pokemon = this.props.pokemon;
+    const { pokemons } = this.props;
+    const { position } = this.state;
+    const currentTypeList = pokemons.filter(poke => poke.type === this.state.type);
+    const pokemon = currentTypeList[position];
+
     return (
-      <div className="pokedex">
-        <Pokemon key={pokemon.id} pokemon={pokemon} />
+      <div>
+        <button onClick={() => this.nextPokemon(currentTypeList)}>Next {'>>'}</button>
+        <div className="pokedex">
+          <Pokemon key={pokemon.id} pokemon={pokemon} />
+        </div>
+        <div className="filterButtons">
+          <div className="typeButton" id="Fire" onClick={this.typeSelect}>Fire</div>
+          <div className="typeButton" id="Psychic" onClick={this.typeSelect}>Psychic</div>
+        </div>
       </div>
     );
   }
