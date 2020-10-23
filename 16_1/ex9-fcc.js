3f8f4f83f95e52217fb5f309dbf7c5b6462b8c27
FlavioSugano <flavio.sugano@gmail.com>
2020-10-22 22:47:45 -0300

    Ex 6 a 10

---
+++ 16_1/ex9-fcc.js
@@ -0,0 +1,30 @@
+// https://www.freecodecamp.org/learn/front-end-libraries/redux/write-a-counter-with-redux
+
+// In this lesson, you'll implement a simple counter with Redux from scratch. The basics are provided in
+// the code editor, but you'll have to fill in the details! Use the names that are provided and define
+// incAction and decAction action creators, the counterReducer(), INCREMENT and DECREMENT action types, and
+// finally the Redux store. Once you're finished you should be able to dispatch INCREMENT or DECREMENT actions
+// to increment or decrement the state held in the store. Good luck building your first Redux app!
+
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for decrement action types
+
+const counterReducer = (state = 0, { type, value }) => {
+  switch(type) {
+    case INCREMENT: 
+      return state + value;
+    case DECREMENT:
+      return state - value;
+    default: return state;
+  }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+  return { type: INCREMENT, value: 1 }
+}; // Define an action creator for incrementing
+
+const decAction = () => {
+  return { type: DECREMENT, value: 1 }
+}; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
